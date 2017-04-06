994b7ea57347ef8710d0f0d5fbfd143a3c407fea
Ricardo Mejía <mejiaro@gmail.com>
2017-04-06 09:15:28 -0600

    set up additional rspec config

---
+++ spec/spec_helper.rb
@@ -0,0 +1,39 @@
+require 'database_cleaner'
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
+
+RSpec.configure do |config|
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+
+  config.shared_context_metadata_behavior = :apply_to_host_groups
+
+  #adds factory girl methods
+  config.include FactoryGirl::Syntax::Methods
+
+
+  #Database Cleaner setup from example
+  #ensure test db is always on a clean state
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    #transactions are rolled back at end of tests
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
+
+end
