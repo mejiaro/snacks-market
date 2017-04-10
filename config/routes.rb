a23d64c4011ae58048e0497f63b7598b34ef0a12
Ricardo Mejía <mejiaro@gmail.com>
2017-04-09 22:07:29 -0600

    re route the like action to a more descriptive one

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :products do
   	get 'search', on: :collection
+  	post 'like', on: :member
   end
-  #get 'products/search', to: 'products#search'
-  post 'products/like', to: 'products#like'
   post 'auth/login', to: 'authentication#authenticate'
 end
