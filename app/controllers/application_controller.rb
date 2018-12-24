778d8d34846ab3d1507132abd5d5907b3a36b90f
Ricardo Mejía <mejiaro@gmail.com>
2018-12-24 14:51:37 -0600

    remove custom auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,3 @@
 class ApplicationController < ActionController::API
   include Response
-  include ExceptionHandler
-  include AdminAuthorization
-
-  before_action :authorize_request
-
-  attr_reader :current_user
-
-  private
-
-  def authorize_request
-  	@current_user = (AuthorizeApiRequest.new(request.headers).call)[:user]
-  end
 end
